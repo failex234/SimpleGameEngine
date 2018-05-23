6421b3c2f0ecceefa71d069ad981e21314f7ce57
Felix Naumann <felix@emilnaumann.de>
2018-05-24 01:22:07 +0200

    almost finished circle, some dots are missing

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all:
-	g++ -c src/Game.cpp
-	g++ -o game Game.o src/main.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+	g++ -c -g src/Game.cpp
+	g++ -o game Game.o -g src/main.cpp -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 
 run:
 	./game
--- src/Game.cpp
+++ src/Game.cpp
@@ -6,11 +6,12 @@
 #include <cmath>
 #include "Game.hpp"
 
-Game::Game( uint width, uint height, std::string title ) : window({width, height}, title, sf::Style::Titlebar | sf::Style::Close) {
+Game::Game(uint width, uint height, std::string title) : window({width, height}, title,
+                                                                sf::Style::Titlebar | sf::Style::Close) {
 
     window.setFramerateLimit(30);
 
-    if(player_texture.loadFromFile("assets/rocket.png")) {
+    if (player_texture.loadFromFile("assets/rocket.png")) {
         player_texture.setSmooth(true);
         player_texture.setRepeated(false);
         player_sprite.setTexture(player_texture);
@@ -30,7 +31,7 @@ Game::Game( uint width, uint height, std::string title ) : window({width, height
     text.setPosition(5, 5);
 }
 
-void Game::run( void ) {
+void Game::run(void) {
 
     player.angle = 0;
 
@@ -39,7 +40,7 @@ void Game::run( void ) {
 
     coinsrendered = false;
 
-    while(window.isOpen()) {
+    while (window.isOpen()) {
         render.clear();
         readInputs();
         //move();
@@ -49,28 +50,28 @@ void Game::run( void ) {
     }
 }
 
-void Game::readInputs( void ) {
+void Game::readInputs(void) {
     sf::Event event{};
 
-    while(window.pollEvent(event)) {
+    while (window.pollEvent(event)) {
         if (event.type == sf::Event::Closed) {
             window.close();
         }
     }
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-	    if (player.angle < 270) {
-		for (int i = player.angle; i <= 270; i++) {
-			player.angle = i;
-			sf::milliseconds(500);
-		}
-	    } else {
-		for (int i = player.angle; i >= 270; i--) {
-			player.angle = i;
-			sf::milliseconds(500);
-		}	
-	    }
-	    //player.angle = 270.0f;
+        if (player.angle < 270) {
+            for (int i = player.angle; i <= 270; i++) {
+                player.angle = i;
+                sf::milliseconds(500);
+            }
+        } else {
+            for (int i = player.angle; i >= 270; i--) {
+                player.angle = i;
+                sf::milliseconds(500);
+            }
+        }
+        //player.angle = 270.0f;
         if (player.y >= 40) {
             player.y -= 20;
         }
@@ -78,27 +79,27 @@ void Game::readInputs( void ) {
     }
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-	    player.angle = 90.0f;
+        player.angle = 90.0f;
         if (player.y <= window.getSize().y - 40) {
             player.y += 20;
         }
     }
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-	    player.angle = 180.0f;
+        player.angle = 180.0f;
         if (player.x >= 40) {
             player.x -= 20;
         }
     }
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-	    player.angle = .0f;
+        player.angle = .0f;
         if (player.x <= window.getSize().x - 40) {
             player.x += 20;
         }
     }
 
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)) {
         if (player.angle <= 320) {
             player.angle += 90;
         } else {
@@ -125,7 +126,7 @@ void Game::readInputs( void ) {
 
 }
 
-void Game::checkCollision( void ) {
+void Game::checkCollision(void) {
 
     int i = 0;
 
@@ -142,9 +143,10 @@ void Game::checkCollision( void ) {
 
 }
 
-void Game::draw( void ) {
+void Game::draw(void) {
 
     window.clear(sf::Color::White);
+    render.clear();
 
     text.setString("Player coordinates"
                    "\nX: " + std::to_string(player.x) +
@@ -165,31 +167,51 @@ void Game::draw( void ) {
         coinsrendered = true;
     }
 
-    for(auto &coin : coins) {
+    for (auto &coin : coins) {
         coin.sprite.setPosition(coin.x, coin.y);
         window.draw(coin.sprite);
     }
 
+    drawCircle(window.getSize().x / 2, window.getSize().y / 2, 100, sf::Color::Black);
+
     window.draw(player_sprite);
+    window.draw(render.data(), render.size(), sf::Points);
 
     window.display();
 
 }
 
-void Game::drawCircle( int x, int y, int rad, sf::Color color ) {
+void Game::drawCircle(int x, int y, int rad, sf::Color color) {
     //Add top most point of circle to render array
     render.push_back(sf::Vertex({x, y + rad}, color));
 
     //Calculate all points from top-most to right-most point
-    for(int i = 1; i <= rad; i++) {
-        render.push_back(sf::Vertex({i, calculateY(x, rad)}, color));
+    for (int i = 1; i <= rad; i++) {
+        render.push_back(sf::Vertex({x + i, y - calculateY(i, rad)}, color));
     }
 
     //Add right-most point to render array
     render.push_back(sf::Vertex({x + rad, y}, color));
 
     //Calculate all points from right-most to bottom-most point
+    for (int i = 1; i <= rad; i++) {
+        render.push_back(sf::Vertex({x + calculateX(i, rad), y + i}, color));
+    }
 
+    //Add bottom-most point to render array
+    render.push_back(sf::Vertex({x, y - rad}, color));
+
+    //Calculate all points from bottom-most to left-most point
+    for (int i = 1; i <= rad; i++) {
+        render.push_back(sf::Vertex({x - i, y + calculateY(-i, rad)}, color));
+    }
+
+    //Add left-most point to render array
+    render.push_back(sf::Vertex({x - rad, y}, color));
+
+    for (int i = 1; i <= rad; i++) {
+        render.push_back(sf::Vertex({x - calculateX(-i, rad), y - i}, color));
+    }
 
 }
 
@@ -199,8 +221,12 @@ void Game::drawCircle( int x, int y, int rad, sf::Color color ) {
  * @param rad radius
  * @return point y
  */
-int Game::calculateY( int x, int rad ) {
-
+int Game::calculateY(int x, int rad) {
+    if (rad != 0) {
+        return sqrt(pow(rad, 2) - pow(x, 2));
+    } else {
+        return 1337;
+    }
 }
 
 /**
@@ -209,15 +235,19 @@ int Game::calculateY( int x, int rad ) {
  * @param rad radius
  * @return point x
  */
-int Game::calculateX( int y, int rad ) {
+int Game::calculateX(int y, int rad) {
+    if (rad != 0) {
+        return sqrt(pow(rad, 2) - pow(y, 2));
+    } else {
+        return 1337;
+    }
+}
+
+void Game::checkVals(void) {
 
 }
 
-void Game::checkVals( void ) {
-
-}
-
-Coin Game::randomCoin( void ) {
+Coin Game::randomCoin(void) {
     Coin coin;
 
     coin.x = (rand() % window.getSize().x - 40) + 20;
